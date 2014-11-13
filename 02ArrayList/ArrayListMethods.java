d6792c8534821c54eddeb9b4cd07a719cb03770e
Wei Wu <weihwu@yahoo.com>
2014-11-13 18:47:30 -0500

    Done

--- 02ArrayList/ArrayListMethods.java
+++ 02ArrayList/ArrayListMethods.java
@@ -3,10 +3,9 @@ import java.util.*;
 public class ArrayListMethods{
   
     public static void collapseDuplicates(ArrayList<Integer> L){
-	ArrayList<Integer> array = new ArrayList<Integer>();
 	int a = L.get(0);
 	int i = 1;    
-	while(i < array.size()){
+	while(i < L.size()){
 	    if(L.get(i)==a){
 		L.remove(i);
 	    }
@@ -19,10 +18,26 @@ public class ArrayListMethods{
 
     public static void randomize(ArrayList<Integer> L){
 	Random r = new Random();
-	for(int i = L.size(); i > 0; i--){
-	    int place = r.nextInt(i);
-	    L.add(L.get(place), L.size()-1);
+	for(int i = 0; i < L.size(); i++){
+	    int place = r.nextInt(L.size()-i)+i;
+	    L.add(0, L.get(place));
 	    L.remove(place+1);
 	}
+    }
+
+    public static void main(String[]args){
+	ArrayList<Integer> array = new ArrayList<Integer>();
+	for (int x = 0; x < 10; x++){
+	    array.add(x);
+	    array.add(x);
+	    array.add(x);
+	}
+	System.out.println(array);
+	collapseDuplicates(array);
+	System.out.println(array);
+	randomize(array);
+	System.out.println(array);
+	randomize(array);
+	System.out.println(array);
     }
 }
