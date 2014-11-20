931577202aeed52560d38b8088b8e26bfdfbb74e
Wei Wu <weihwu@yahoo.com>
2014-11-19 22:53:04 -0500

    It now works with html comments

--- 03WordSearch/WordGrid.java
+++ 03WordSearch/WordGrid.java
@@ -1,14 +1,22 @@
 public class WordGrid{	
     public static void main(String[]args){
 	WordGrid grid1 = new WordGrid(4,5);
+	grid1.clear();
+      	grid1.addWordHorizontal("cat", 1, 1);
+	grid1.addWordHorizontal("dog", 2, 1);
+	grid1.addWordHorizontal("cow", 3, 1);
+	grid1.addWordHorizontal("pig", 2, 1);
 	System.out.println(grid1.toString());
     }
+
     private char[][]data;
     
+    /** The default constructor to set the size of the grid */
     public WordGrid(int rows, int cols){
 	data = new char[rows][cols];
     }
     
+    /** Clears the grid, making all places an empty space */
     public void clear(){
 	for(int i = 0; i < data.length; i++){
 	    for(int j = 0; j < data[i].length; j++){
@@ -17,6 +25,7 @@ public class WordGrid{
 	}
     }
     
+    /** Turns the double array into a formatted string, characters separated by spaces */
     public String toString(){
 	String grid = "";
 	for(int i = 0; i < data.length; i++){
@@ -28,16 +37,27 @@ public class WordGrid{
 	return grid;
     }
 
+    /** Checks to see the eligibility and to add the word the user wants, including in which row and column */
     public boolean addWordHorizontal(String word, int row, int col){
-	if ((data[row].length - col) < word.length()){
+	if (row >= data.length){
 	    return false;
 	}
+	if ((data[row].length - col) < word.length()){	 
+	    return false;
+	}
+	int place1 = 0;
 	for(int i = col-1; i < word.length(); i++){
-	    if ((data[row][i] == ' ') || (data[row][i] == word.charAt(i - col + 1))){
-		data[row][i] = word.charAt(i - col + 1);	 
-	    }else{
-		return false;
-	    }
+	    if (data[row][i] != ' '){
+		if (data[row][i] != word.charAt(place1)){
+		    return false;
+		}
+	    }	 
+	    place1++;
+	}
+	int place2 = 0;
+	for(int i = col-1; i < word.length(); i++){
+	    data[row][i] = word.charAt(place2);	
+	    place2++;
 	}
 	return true;
     }
