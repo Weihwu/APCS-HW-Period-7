2d16dd90ca6ef3d4a21b96a1ea10315c5a47dc58
Weihwu <weihwu@yahoo.com>
2014-12-01 13:14:19 -0500

    more stuff to do

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,4 +1,11 @@
 public OrderedSuperArray extends SuperArray{
     
-    public void add{
-	
+    public void add(String e){
+	int index = 0;
+	for(int x; x < superA.length; x++){
+	    if (((x.charAt(0)).compareTo(superA[x].charAt(0))) == 1){
+		add(index, e);
+	    }
+	    index++;
+	}
+    }
