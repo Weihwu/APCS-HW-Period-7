4d6f4583c1fe4fe5352fcc8980e36fadb813b698
Weihwu <weihwu@yahoo.com>
2014-12-02 13:17:15 -0500

    this is classwork

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,25 +1,17 @@
 public class OrderedSuperArray extends SuperArray{
+
     public void add(String e){
 	if (size() == 0){
 	    super.add(e);
 	}else{
-	    int index = 0;
-	    int place = 0;
 	    for(int x = 0; x < size(); x++){
-		if (e.compareTo(get(x)) == -1){
-		    super.add(index, e);
-		}else if(e.compareTo(get(x)) <= 0){
-		    place++;
-		    index = 0;
-		}else if(e.compareTo(get(x)) == 1){
-		    super.add(index, e);
-		    return;
+		if(get(x).compareTo(e) > 0 && get(x+1).compareTo(e) < 0){
+		    super.add(x, e);
 		}
-		index++;
 	    }
-	    super.add(e);
 	}
     }
+
     public String set(int index, String e){
 	String holder  = get(index);
 	remove(index);
