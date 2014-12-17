76d0b5977a9262fab72f35479c8d54a61b893be2
Weihwu <weihwu@yahoo.com>
2014-12-17 13:07:26 -0500

    All Sorts Tested

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,4 +1,41 @@
+import java.util.*;
+
 public class Sorts{
+    public static void main(String[]args){
+	int[] target1 = new int[50000];
+	for(int x = 0; x < 50000; x++){
+	    target1[x] = (int)(Math.random() * 50000);
+	}
+
+	int[] target2 = new int[50000];
+	for(int x = 0; x < 50000; x++){
+	    target2[x] = target1[x];
+	}
+
+	int[] target3 = new int[50000];
+	for(int x = 0; x < 50000; x++){
+	    target3[x] = target1[x];
+	}
+
+	System.out.println("Insertion Sort:");
+	long startTime = System.nanoTime();
+	insertionSort(target1);
+	long endTime = System.nanoTime();
+	System.out.println((endTime - startTime) + " nanoseconds");
+
+	System.out.println("Selection Sort:");
+	startTime = System.nanoTime();
+	selectionSort(target2);
+	endTime = System.nanoTime();
+	System.out.println((endTime - startTime) + " nanoseconds");
+	
+	System.out.println("Bubble Sort:");
+	startTime = System.nanoTime();
+	bubbleSort(target3);
+	endTime = System.nanoTime();
+	System.out.println((endTime - startTime) + " nanoseconds");
+    }
+
     public static void bubbleSort(int[] c){
 	int place = c.length;
 	for(int x = 0; x < place; x++){
@@ -11,7 +48,7 @@ public class Sorts{
 	}
     }
 
-    public void insertionSort(int[] c){
+    public static void insertionSort(int[] c){
 	for(int x = 0; x < c.length; x++){
 	    int place = x;
 	    while (place > 0 && c[place] < c[place-1]){
@@ -23,7 +60,7 @@ public class Sorts{
 	}
     }
 
-    public void selectionSort(int[] c){
+    public static void selectionSort(int[] c){
 	for(int i = 0; i < c.length; i++){
 	    int min = c[i];
 	    int target = i;
