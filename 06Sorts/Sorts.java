6e58841b97abe5b868de3584fa458de6261838c0
Wei Wu <weihwu@yahoo.com>
2014-12-20 20:44:26 -0500

    all sorts done

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -23,39 +23,51 @@ public class Sorts{
 	    target4[x] = target1[x];
 	}
 
+	int[] target5 = new int[w    ];
+	for(int x = 0; x < w    ; x++){
+	    target5[x] = target1[x];
+	}
+
 	System.out.println("Insertion Sort:");
 	long startTime = System.nanoTime();
 	insertionSort(target1);
 	long endTime = System.nanoTime();
 	System.out.println((endTime - startTime) + " nanoseconds");
-	System.out.println(target1);
+	System.out.println(toString(target1));
 
 	System.out.println("Selection Sort:");
 	startTime = System.nanoTime();
 	selectionSort(target2);
 	endTime = System.nanoTime();
 	System.out.println((endTime - startTime) + " nanoseconds");
-	System.out.println(target2);
+	System.out.println(toString(target2));
 	
 	System.out.println("Bubble Sort:");
 	startTime = System.nanoTime();
 	bubbleSort(target3);
 	endTime = System.nanoTime();
 	System.out.println((endTime - startTime) + " nanoseconds");
-	System.out.println(target3);
+	System.out.println(toString(target3));
+
+	System.out.println("Radix Sort:");
+	startTime = System.nanoTime();
+	radixSort(target4);
+	endTime = System.nanoTime();
+	System.out.println((endTime - startTime) + " nanoseconds");
+	System.out.println(toString(target4));
 
 	System.out.println("Regular Sort:");
 	startTime = System.nanoTime();
-	Arrays.sort(target3, 0, target3.length);
+	Arrays.sort(target5);
 	endTime = System.nanoTime();
 	System.out.println((endTime - startTime) + " nanoseconds");
-	System.out.println(target4);
+	System.out.println(toString(target5));
     }
 
     public static void bubbleSort(int[] c){
 	for(int i = 0; i < c.length; i++){
 	    for(int j = 0; j < c.length - 1; j++){
-		if ([j] > c[j+1]){
+		if (c[j] > c[j+1]){
 		    c[j] = c[j] + c[j+1];
 		    c[j + 1] = c[j] - c[j+1];
 		    c[j] = c[j] - c[j+1];
@@ -102,21 +114,29 @@ public class Sorts{
 		temp = c[i];
 	    }
 	    while(temp/exp > 0){
-		int[] buckets = new int[10];
+		int[] bucket = new int[10];
 		for(i = 0; i < c.length; i++){
 		    bucket[(c[i]/exp) % 10]++;
 		}
 		for(i = 1; i < 10; i++){
 		    bucket[i] += bucket[i-1];
 		}
-		for(i = n-1; i >= 0; i--){
-		    b[--bucket[(a[i]/exp) % 10]] = a[i];
+		for(i = c.length-1; i >= 0; i--){
+		    b[--bucket[(c[i]/exp) % 10]] = c[i];
 		}
-		for(i = 0; i < n; i++){
+		for(i = 0; i < c.length; i++){
 		    c[i] = b[i];
 		}
 		exp *= 10;
 	    }
 	}
+    }
+    public static String toString(int[] c){
+	String str = "[";
+	for(int x = 0; x < c.length; x++){
+	    str += " " + c[x];
+	}
+	str += " ]";
+	return str;
     }
 }
